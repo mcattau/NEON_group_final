a2a070691663a2af1a2bb6637da95b6047b40ab5
sjmc <sjmc@users.noreply.github.com>
2016-06-24 14:30:16 -0600

    adding draft pretty Rmd

--- Scripts/plots_ts_20160624_sjer.R
+++ Scripts/plots_ts_20160624_sjer.R
@@ -17,12 +17,12 @@ setwd("C:/Users/stellac/Box Sync/Work/neonwksp/data/NEONDI-2016/")
 
 ###{r load-me}
 # set parameters
-site_name <- "SJERroot"
-site_file_code <- "SJER"
+site_name <- "SOAProot"
+site_file_code <- "SOAP"
 output_dir_path <- "../outputs/"
 
 #load the raster
-SITE_chm <- raster("../NEONDI-2016/NEONdata/D17-California/SJER/2013/lidar/SJER_lidarCHM.tif")
+SITE_chm <- raster("../NEONDI-2016/NEONdata/D17-California/SOAP/2013/lidar/SOAP_lidarCHM.tif")
 
 ###{r inspect-and-set-values-to-na, echo=FALSE}
 #
@@ -82,6 +82,11 @@ polys <- SpatialPolygons(mapply(function(poly, ID) {
 
 stem.polys.spdf <- SpatialPolygonsDataFrame(polys, data.frame(id=ID, row.names=ID))
 stem.polys.spdf
+stem.polys.spdf@plotOrder
+stem.polys.spdf@data
+ID
+polys$
+
 X11()
 plot(stem.polys.spdf, main = "stem.polys.spdf")
 
@@ -90,7 +95,7 @@ plot(stem.polys.spdf, main = "stem.polys.spdf")
 # Note that below will return a dataframe containing the max height
 # calculated from all pixels in the buffer for each plot
 SITE_height_mean <- extract(SITE_chm,
-                            polys,
+                            stem.polys.spdf,
                             fun=mean ,
                             na.rm = TRUE,
                             sp=TRUE, # spatial object with max values, not just vals.
@@ -98,18 +103,19 @@ SITE_height_mean <- extract(SITE_chm,
 
 SITE_height_mean # this is a spatial points data frame
 
-ht_mean <- data.frame(SITE_height_mean$id, SITE_height_mean$SJER_lidarCHM )
+ht_mean <- data.frame(SITE_height_mean$id, SITE_height_mean$SOAP_lidarCHM )
+ht_mean
 
 SITE_height_max <- extract(SITE_chm,
-                           polys,
+                           stem.polys.spdf,
                            fun=max ,
                            na.rm = TRUE,
                            sp=TRUE, # spatial object with max values, not just vals.
                            stringsAsFactors=FALSE)
 
-SITE_height_max$SJER_lidarCHM
+SITE_height_max$SOAP_lidarCHM
 
-ht_max <- data.frame(SITE_height_max$id, SITE_height_max$SJER_lidarCHM )
+ht_max <- data.frame(SITE_height_max$id, SITE_height_max$SOAP_lidarCHM )
 
 ht_mean
 ht_max
@@ -117,18 +123,20 @@ ht_max
 ###{r import-in-situ}
 # import the centroid data and the vegetation structure data
 #if this breaks check veg-structure vs. veg_structure
-SITE_insitu <- read.csv("NEONdata/D17-California/SJER/2013/insitu/veg_structure/D17_2013_SJER_vegStr.csv")
+SITE_insitu <- read.csv("C:/Users/stellac/Box Sync/Work/neonwksp/data/NEONDI-2016/NEONdata/D17-California/SOAP/2013/insitu/veg-structure/D17_2013_SOAP_vegStr.csv")
 SITE_insitu$plotid
 #replace plotid with ID so it matches what we want to join by
-SITE_insitu$ID <- substr(SITE_insitu$plotid,start=5,stop=10)
-ID_insitu <-unique(SITE_insitu$ID )
-SITE_insitu$ID
-ID_insitu; ID
+#SITE_insitu$ID <- substr(SITE_insitu$plotid,start=5,stop=10)
+#ID_insitu <-unique(SITE_insitu$ID )
+#SITE_insitu$ID
+#ID_insitu; ID
 
 #18 plots in the insitu data
-insitu_maxStemHeight <- ddply(SITE_insitu,.(ID),summarize,max = max(stemheight,na.rm=T))
-insitu_meanStemHeight <- ddply(SITE_insitu,.(ID),summarize,max = mean(stemheight,na.rm=T))
+library(plyr)
+insitu_maxStemHeight <- ddply(SITE_insitu,.(plotid),summarize,max = max(stemheight,na.rm=T))
+insitu_meanStemHeight <- ddply(SITE_insitu,.(plotid),summarize,max = mean(stemheight,na.rm=T))
 
+detach("package:plyr", unload=TRUE)
 # let's create better, self documenting column headers
 names(insitu_maxStemHeight) <- c("plotid","insituHtValue")
 head(insitu_maxStemHeight)
@@ -151,7 +159,7 @@ SITE_height_mean@data <- data.frame(SITE_height_mean@data,
 SITE_height_mean@data$insituHtValue
 
 ###{r describe-me}
-ggplot(SITE_height_max@data, aes(x=SJER_lidarCHM, y = insituHtValue)) +
+ggplot(SITE_height_max@data, aes(x=SOAP_lidarCHM, y = insituHtValue)) +
   geom_point() +
   theme_bw() +
   ylab("Maximum measured height") +
@@ -161,7 +169,7 @@ ggplot(SITE_height_max@data, aes(x=SJER_lidarCHM, y = insituHtValue)) +
 
 ###{r plot-gg-regression}
 ##plot with regression fit
-p <- ggplot(SITE_height_max@data, aes(x = SJER_lidarCHM, y = insituHtValue)) +
+p <- ggplot(SITE_height_max@data, aes(x = SOAP_lidarCHM, y = insituHtValue)) +
   geom_point() +
   ylab("Maximum Measured Height") +
   xlab("Maximum LiDAR Height") +
@@ -169,7 +177,7 @@ p <- ggplot(SITE_height_max@data, aes(x = SJER_lidarCHM, y = insituHtValue)) +
   geom_smooth(method = lm)
 
 p + theme(panel.background = element_rect(colour = "grey")) +
-  ggtitle("SJER LiDAR CHM Derived vs Measured Tree Height") +
+  ggtitle("SOAP LiDAR CHM Derived vs Measured Tree Height") +
   theme(plot.title=element_text(family="sans", face="bold", size=20, vjust=1.9)) +
   theme(axis.title.y = element_text(family="sans", face="bold", size=14, angle=90,   hjust=0.54, vjust=1)) +
   theme(axis.title.x = element_text(family="sans", face="bold", size=14, angle=00, hjust=0.54, vjust=-.2))
